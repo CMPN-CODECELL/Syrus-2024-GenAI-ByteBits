0b784e6dc72e4aea67110802a9c62e85e5c09c7d
“Shivam <sg82480@gmail.com>
2024-02-20 13:59:00 +0530

    updated assistant

--- app/(browse)/ai-assistant/page.tsx
+++ app/(browse)/ai-assistant/page.tsx
@@ -1,96 +1,72 @@
 "use client"
-import React, { useEffect, useState } from 'react';
-import axios from 'axios'; 
-import "regenerator-runtime";
-import speech,{useSpeechRecognition} from "react-speech-recognition"
 
-const VoiceAssistant: React.FC = () => {
-  const [voiceOutput, setVoiceOutput] = useState<string[]>([]);
-  const { listening, transcript } = useSpeechRecognition();
-  const [voiceInput, setVoiceInput] = useState<string[]>([]);
-  const [voices, setVoices] = useState<string>('');
-  const [imageURL, setImageURL] = useState<string | undefined>();
+import { ModeToggle } from '@/components/theme-toggle'
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import React, {useState } from 'react'
+import { Rings } from 'react-loader-spinner'
 
-  const sendVoiceInputToBackend = async (input: string) => {
-    try {
-      const response = await axios.post('http://127.0.0.1:5000/', { input }, {
-        headers: {
-          'Content-Type': 'application/json', 
-        },
-      });
-      console.log('Backend Response:', response.data);
-      if (response?.data) {
-        setVoiceOutput(prevOutput => [...prevOutput, response.data.text]);
-        if (response?.data?.url) {
-          setImageURL(response.data.url);
-        }
-        const speechSynthesis = window.speechSynthesis;
-        const utterance = new SpeechSynthesisUtterance(response.data.text);
-        // utterance.voice = speechSynthesis.getVoices().find(voice => voice.name === 'Google हिन्दी');
-        speechSynthesis.speak(utterance);
-      }
-    } catch (error) {
-      console.error('Error sending voice input to backend:', error);
-    }
-  };
+const HomePage = () => {
 
-  useEffect(() => {
-    if (!listening && transcript) {
-      setVoiceInput(prevInput => [...prevInput, transcript]);
-      sendVoiceInputToBackend(transcript);
-    }
-  }, [listening, transcript]);
+  const [volume, setVolume] = useState(0.8);
+  const [mirrored, setmirrored] = useState(true);
+  const [loading, setLoading] = useState(false)
 
+  
   return (
-    <>
-      <div className="bg-gray-800 text-white py-2 text-center">
-        <p>Virtual Teacher Assistant</p>
+    
+    <div className="flex h-screen max-lg:flex-col max-md:flex-col">
+      {/* Left divison - webcam and canvas */}
+      <div>
+        
       </div>
-      <div className="flex flex-col h-screen justify-between">
-        <div className="flex justify-center items-center h-full bg-gray-900 text-white">
-          <div className="w-3/4 sm:w-1/2 lg:w-1/3 xl:w-1/4 p-8 bg-white rounded-lg shadow-lg">
-            <h2 className="text-lg font-semibold mb-4">Virtual Classroom</h2>
-            <div className="flex justify-center items-center mb-4">
-              <div className="w-40 h-40 bg-gray-300 rounded-full"></div>
-            </div>
-            <div className='flex justify-start gap-2 items-center'>
-              <button className="bg-blue-700 p-2 m-2" onClick={() => {
-                speech.startListening();
-              }}>Speak</button>
-              <button className="bg-blue-700 p-2 m-2" onClick={() => {
-                speech.stopListening();
-              }}>Stop</button>
-              <button className="bg-blue-700 p-2 m-2" onClick={() => {
-                speechSynthesis.cancel();
-              }}>Stop answer</button>
-            </div>
-            {
-              listening ? <p className='text-black'>Go Ahead i am listening</p> : <p className='text-black'>Click the button and ask me anything</p>
-            }
+      
+      {/* Right divsion - container for button panel and wiki section */}
+      <div className="flex flex-row flex-1">
+        <div className="border-primary/5 border-2 max-w-xs flex flex-col gap-2 justify-between shadow-md rounded-md p-4">
+          {/* Top section */}
+          <div className="flex flex-col gap-2">
+            <ModeToggle/>
+            <Separator className='my-2'/>
           </div>
-          {imageURL && (
-            <div>
-              <img src={imageURL} alt="image" className="w-[50%] h-[50%]" />
-            </div>
-          )}
+
+          {/* Middle section */}
+          <div className="flex flex-col gap-2">
+            <Separator className='my-2'/>
+          
+        
+            <Separator className='my-2'/>
+          </div>
+
+          {/* Bottom section */}
+
+          <div className="flex flex-col gap-2 justify-center items-center">
+            <Separator className='my-2'/>
+
+          </div>
         </div>
-        {
-          voiceInput?.map((question, index) => (
-            <div key={index}>
-              <p><span>Student:</span> {question}</p>
-            </div>
-          ))
-        }
-        {
-          voiceOutput?.map((answer, index) => (
-            <div key={index}>
-              <p><span>Teacher:</span> {answer}</p>
-            </div>
-          ))
-        }
+        <div className="h-full flex-1 py-4 px-2 overflow-y-scroll">
+          <RenderFeatureHighlightSection/>
+        </div>
+      </div>
+      {loading && (
+        <div className="z-50 absolute w-full h-full flex items-center justify-center bg-primary-foreground">
+          Getting things ready . . . <Rings height={50} color='red'/>
+        </div>
+      )}
+    </div>
+  )
+
+  function RenderFeatureHighlightSection(){
+    return (
+    <>
+      <div className="">
+        Hello
       </div>
     </>
-  );
-};
+    )
+  }
 
-export default VoiceAssistant;
+}
+
+export default HomePage
